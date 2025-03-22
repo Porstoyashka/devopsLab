734a1c559d1ca20a3d3741b8cc704100bb7e11fc
Porstoyashka <maksym33kostenko@gmail.com>
2025-03-22 10:20:13 +0200

    Initial commit with FuncA returning 0

---
+++ FuncA.h
@@ -0,0 +1,10 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+public:
+    // Функція обчислення тригонометричної функції (повертає 0 за замовчуванням)
+    double calculate();
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    FuncA func;
+    std::cout << "FuncA result: " << func.calculate() << std::endl;
+    return 0;
+}
